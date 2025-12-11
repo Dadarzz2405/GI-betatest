28e76627804492a40e69c4585acbe94b3965cb5e
Haidar Ali Fawwaz <Haidar-10A@mbas-MacBook-Air-16.local>
2025-12-11 08:06:57 +0700

    Initial Render deployment

---
+++ app.py
@@ -0,0 +1,89 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager, login_user, logout_user, login_required, current_user
+from flask_bcrypt import Bcrypt
+
+from models import db, User
+import os
+
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = 'c0585ef7ad68d55b7fd83abf82d9e93cbd3af7bfb6702710f55c4b16e3fb0a74'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database.db')
+
+db.init_app(app)
+bcrypt = Bcrypt(app)
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        user = User.query.filter_by(username=username).first()
+        if user and bcrypt.check_password_hash(user.password, password):
+            login_user(user)
+            if user.role in ['admin', 'ketua', 'pembina']:
+                return redirect(url_for('dashboard_admin'))
+            else:
+                return redirect(url_for('dashboard_member'))
+        else:
+            return "Invalid credentials"
+        
+    return render_template('login.html')
+
+
+@app.route('/')
+def home():
+    return redirect(url_for('login'))
+
+@app.route('/dashboard_admin')
+@login_required
+def dashboard_admin():
+    if not current_user.role in ['admin', 'ketua', 'pembina']:
+        return "Access denied"
+    return render_template('dashboard_admin.html')
+
+@app.route('/dashboard_member')
+@login_required
+def dashboard_member():
+    if current_user.role in ['admin', 'ketua', 'pembina']:
+        return redirect(url_for('dashboard_admin'))
+    return render_template('dashboard_member.html')
+
+@app.route('/add-member', methods=['GET', 'POST'])
+@login_required
+def add_member():
+    if not current_user.role in ['admin', 'ketua', 'pembina']:
+        return "Access denied"
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        name = request.form['name']
+        class_name = request.form['class_name']
+        role = request.form['role']
+        hashed_password = bcrypt.generate_password_hash(password).decode('utf-8')
+        new_user = User(username=username, password=hashed_password, name=name, class_name=class_name, role=role)
+        db.session.add(new_user)
+        db.session.commit()
+        return redirect(url_for('dashboard_admin'))
+    return render_template('add_member.html')
+
+@app.route('/member-list')
+@login_required
+def member_list():
+    users = User.query.all()
+    return render_template('member_list.html', users=users)
+
+if __name__ == '__main__':
+    with app.app_context():
+        db.create_all()
+    app.run(debug=True)
---
+++ models.py
@@ -0,0 +1,24 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+
+db = SQLAlchemy()
+
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(150), unique=True)
+    password = db.Column(db.String(255))
+    name = db.Column(db.String(150))
+    class_name = db.Column(db.String(50))
+    role = db.Column(db.String(50))  
+
+class Session(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(150))
+    date = db.Column(db.String(50))
+
+class attendance():
+    id = db.Column(db.Integer, primary_key=True)
+    session_id = db.Column(db.Integer)
+    user_id = db.Column(db.Integer)
+    status = db.Column(db.String(50)) 
+    timestamp = db.Column(db.String(50))
